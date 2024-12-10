8b22a20a8a272d8e9f481a54ff1a6093d0104910
Anusha <158035121+Anushabore60@users.noreply.github.com>
2024-12-10 20:16:39 +0530

    Update bottomcomponent.tsx

--- src/app/components/bottomcomponent.tsx
+++ src/app/components/bottomcomponent.tsx
@@ -11,10 +11,10 @@ export default function Bottomgrid(){
                 </div>
                   <div className=" bg-pink-100 opacity-70 font-custom2  w-1/2 h-[300px] rounded-xl text-black text-2xl pt-10 relative ">
                     <div className="pl-12">
-                       S’inscrire & économiser <span className="text-[#5CD2DD;] pl-1">10%</span>
+                       S&apos inscrire & économiser <span className="text-[#5CD2DD;] pl-1">10%</span>
                     </div>
                      <div className=" text-[#BDA2B0] text-sm font-custom2 px-4 py-4 pl-12">
-                        Office ipsum you must be muted. Synergize helicopter prioritize anyway job due harvest most opportunity didn't.
+                        Office ipsum you must be muted. Synergize helicopter prioritize anyway job due harvest most opportunity didn &apost.
                         <br/> Yet busy any meeting shark light marginalised 4-blocker message. Productize corporate nail caught synergy
                         <br/> highlights lunch. Company another pushback items dear or any.
                      </div>
@@ -26,7 +26,7 @@ export default function Bottomgrid(){
                          className="border border-x-2 bg-white rounded-md p-2 text-[#A68A98] text-[18px]"
                           />
                           <button className=" w-1/3 bg-[#5CD2DD] text-white rounded-md  font-custom3 text-lg flex items-center justify-center pl-2 hover:scale-105 ">
-                             S’inscrire
+                             S&apos inscrire
                                <Image  src="/rightarrow.svg" alt="rightarrow" width={22} height={22} className=""/>  
                           </button>
                        </div>
